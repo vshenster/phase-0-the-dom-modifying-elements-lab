77fa64b86deb6860df9bcc6bd3ce5c3278fd4c98
Vicky Shen <vicshe01@gmail.com>
2023-02-27 15:18:00 -0500

    commit tree 844c5282b5e005949fe8406537490541493eb7c9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removeMain = document.querySelector("main#main");
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Vicky is the champion";
